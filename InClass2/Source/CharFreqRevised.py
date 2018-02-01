50ff64b49a45194aa189f2342d9d042da3ef4fd3
vmanvitha3 <vmanvitha3@gmail.com>
2018-01-31 19:13:47 -0600

    Updated code in ICP2
    Updated code in ICP2

--- InClass2/Source/CharFreqRevised.py
+++ InClass2/Source/CharFreqRevised.py
@@ -6,11 +6,15 @@ try:    #if file exists it opens
 except: #if no such file exists it throws an exception and exits
     print ("File cannot be opened",fName)
     exit()
-
+file = open("Output.txt", 'w')  #Open file in write mode
 #it reads each word
 for word in fhandler:
     word = word.strip()     #it strips new line characters and few others
     count_freq = 0         #creates an empty dictionary called count_freq
     for c in word:
         count_freq = count_freq + 1  #for each letter within the word increment count by 1 till no characters left
-    print (word.strip("\n"),",",count_freq)    #Prints result
+    data = word.strip("\n") +","+ str(count_freq)+"\n"  #Store output in a variable called data
+    file.write(data)                 #write data to file
+    #print (word.strip("\n"),",",count_freq)    #Prints result
+
+file.close()        #close file
